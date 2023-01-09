0bc26adf146709272a478b12d96f7ab3778c0a8e
Riba00 <jordiriba@iesebre.com>
2023-01-09 11:11:26 +0100

    Initial commit

---
+++ resources/views/policy.blade.php
@@ -0,0 +1,122 @@
+<x-guest-layout>
+    <main>
+        <div class="relative bg-gray-200 pt-16 pb-4 sm:pt-24 lg:pt-32">
+            <div class="mx-auto max-w-md px-4 text-left sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+                <div>
+                    <p class="mb-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Politica de
+                        privacitat</p>
+
+                    <p class="text-lg text-gray-500 mt-4">En compliment del Capítol II de la llei 34/2002,
+                        LSSICE, els informem que la present pàgina web és propietat de JRiba SL, d’ara
+                        en endavant també el prestador, domiciliada a Major,13 43550 Ulldecona, amb CIF: B01895606,
+                        Telèfon: 689632100 i email info@jriba.com.</p>
+
+                    <p class="text-lg text-gray-500 mt-4">JRiba SL, com a responsable d’aquest lloc web
+                        i en conformitat amb el que disposa la normava vigent en matèria de protecció de dades
+                        personals, el Reglament (UE) 2016/679 de 27 d’abril de 2016 (RGPD) relau a la protecció
+                        de les persones siques pel que fa al tractament de dades personals ia la lliure
+                        circulació d’aquestes dades, la Llei Orgànica 3/2018, de 5 de desembre, de protecció
+                        de dades de caràcter personal (LOPDGDD) I per la Llei 34/2002, de 11 de juliol, de
+                        Serveis de la Societat de la Informació i del Comerç Electrònic (LSSICE), té
+                        implementades totes les mesures de seguretat, d’índole tècnica i organitzaves per
+                        garanr i protegir la confidencialitat, integritat i disponibilitat de les dades
+                        introduïdes.</p>
+
+                    <p class="text-lg text-gray-500 mt-4">A l’efecte del que preveu la LOPDGDD, JRiba SL l’informa que
+                        les dades que voluntàriament ens està facilitant seran incorporades
+                        als nostres sistemes d’informació per tal de realitzar les gesons comercials i
+                        administraves necessàries amb els usuaris del web; Les operacions previstes per a
+                        realitzar els tractaments són els següents: respondre a les consultes i / o
+                        proporcionar informacions requerides per l’Usuari; realitzar les prestacions de serveis
+                        i / o productes contractats o subscrits per l’Usuari; realitzar totes aquelles acvitats
+                        pròpies de JRiba SL pel present avís legal ressenyades I remetre el butlle de
+                        nocies de la pàgina web.</p>
+
+                    <p class="text-lg text-gray-500 mt-4">El prestador s’eximeix de qualsevol tipus de
+                        responsabilitat derivada de la informació publicada al nostre lloc web, sempre que
+                        aquesta informació hagi estat manipulada o introduïda per un tercer aliè.</p>
+
+                    <p class="text-lg text-gray-500 mt-4">JRiba SL no atorga cap garantia ni es fa
+                        responsable, en cap cas, dels danys i perjudicis de qualsevol naturalesa que puguin
+                        sortir a col·lació de la manca de disponibilitat, manteniment i efectiu funcionament del
+                        web o dels seus serveis i continguts; de l’existència de virus, programes maliciosos o
+                        lesius en els continguts; de l’ús il·lícit, negligent, fraudulent o contrari a aquest
+                        Avís Legal I condicions d’ús; o de la falta de licitud, qualitat, fiabilitat, utilitat i
+                        disponibilitat dels serveis prestats per tercers i posats a disposició dels usuaris al
+                        lloc web.</p>
+
+
+                    <h2 class="text-xl font-semibold mt-5 text-cyan-600">Acceptacio i consentiment</h2>
+
+                    <p class="text-lg text-gray-500 mt-4">L’Usuari declara haver estat informat de les condicions
+                        sobre protecció de dades de caràcter personal, acceptant i consennt el tractament
+                        automatzat dels mateixos per part de JRiba SL en la forma i per a les finalitats
+                        indicades en la present Políca de Protecció de Dades Personals.</p>
+
+                    <p class="text-lg text-gray-500 mt-4">A través d’aquesta políca de privacitat li informem que
+                        les fotografies que esguin penjades al web són propietat de JRiba SL, incloent
+                        les dels menors, en les quals, per a l’obtenció d’aquestes, s’ha obngut el consenment
+                        previ dels pares, tutors o representants legals mitjançant la signatura dels formularis
+                        realitzats a l’efecte. No obstant això, els pares, tutors o representants dels menors,
+                        com a tulars de l’exercici dels drets d’aquests, i sempre previ requeriment formal per
+                        escrit, poden indicar la negava a l’ús de la imatge del menor; en aquest cas, la imatge
+                        es mostrarà pixelada.
+                    </p>
+
+                    <h2 class="text-xl font-semibold mt-5 text-cyan-600">Exactitut i veracitat de les dades</h2>
+
+                    <p class="text-lg text-gray-500 mt-4">L’usuari és l’únic responsable de la veracitat i
+                        correcció de les dades que reme a JRiba SL, Exonerant a prestador de qualsevol
+                        responsabilitat al respecte. Els usuaris garanteixen i responen, en qualsevol cas, de
+                        l’exactud, vigència i autencitat de les dades personals facilitades, i es comprometen a
+                        mantenir-les degudament actualitzades. L’usuari accepta proporcionar informació
+                        completa i correcta en el formulari de registre o subscripció.
+                    </p>
+
+                    <h2 class="text-xl font-semibold mt-5 text-cyan-600">Correus comercials</h2>
+
+                    <p class="text-lg text-gray-500 mt-4">D’acord amb la LSSICE, JRiba SL. no realitza
+                        pràcques de SPAM, per tant, no envia correus comercials per e-mail que no hagin estat
+                        prèviament sol·licitats o autoritzats per l’Usuari. En conseqüència, a cada un dels
+                        formularis de la pàgina web, l’Usuari té la possibilitat de donar el seu consenment
+                        per rebre el nostre Butlle, amb independència de la informació comercial puntualment
+                        sol·licitada.
+                    </p>
+
+                    <p class="text-lg text-gray-500 mt-4">Independentment de la finalitat per la qual fossin
+                        destinats, la reproducció total o parcial, ús, distribució i comunicació pública
+                        requereixen de l’autorització escrita prèvia per part del prestador. Qualsevol ús no
+                        autoritzat prèviament per part del prestador serà considerat un incompliment greu dels
+                        drets de propietat intel·lectual o industrial de l’autor.</p>
+
+
+                    <h2 class="text-xl font-semibold mt-5 text-cyan-600"></h2>
+
+
+                    <h2 class="text-xl font-semibold mt-5 text-cyan-600">Veracitat de la informació</h2>
+
+                    <p class="text-lg text-gray-500 mt-4">Tota la informació que facilita l’Usuari ha de ser
+                        veraç. A aquests efectes, l’Usuari garanteix l’autenticitat de les dades comunicades a
+                        través dels formularis de contacte.
+                    </p>
+
+                    <p class="text-lg text-gray-500 mt-4">Serà responsabilitat de l’Usuari mantenir tota la
+                        informació facilitada a JRiba SL permanentment actualitzada de manera que
+                        respongui, en cada moment, a la seva situació real. En tot cas, l’Usuari serà l’únic
+                        responsable de les manifestacions falses o inexactes que realitzi i dels perjudicis que
+                        causi al prestador o a tercers.</p>
+
+                    <h2 class="text-xl font-semibold mt-5 text-cyan-600">Llei aplicable i jurisdicció</h2>
+
+                    <p class="text-lg text-gray-500 mt-4">Amb caràcter general les relacions entre JRiba SL amb
+                        els Usuaris dels seus serveis telemàcs, presents en aquesta web, estan
+                        sotmesos a la legislació i jurisdicció espanyola a la qual se sotmeten expressament
+                        les parts, sent competents per a la resolució de tots els conflictes derivats o
+                        relacionats amb el seu ús els Jutjats i Tribunals.</p>
+                </div>
+            </div>
+        </div>
+    </main>
+    </div>
+    </div>
+</x-guest-layout>
---
+++ resources/views/terms.blade.php
@@ -0,0 +1,135 @@
+<x-guest-layout>
+
+    <main>
+
+        <div class="relative bg-gray-200 pt-16 pb-4 sm:pt-24 lg:pt-32">
+            <div class="mx-auto max-w-md px-4 text-left sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+                <div>
+                    <p class="mb-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Avís legal i
+                        Condicions d'ús</p>
+
+                    <p class="text-lg text-gray-500">Per tal de donar compliment a l’article 10 de la Llei
+                        34/2002 de Serveis de la Societat de la Informació i del Comerç Electrònic, informem als
+                        usuaris de les nostres dades:</br>
+                        </br>
+                        Denominació Social: JRiba SL</br>
+                        Domicili Social: Major,13 43550 Ulldecona </br>
+                        CIF: B01895606</br>
+                        Telèfon: 689632100</br>
+                        E-Mail: info@jriba.com</br>
+                        Lloc Web: jriba.me</br>
+                    </p>
+
+                    <h2 class="text-xl font-semibold mt-5 text-cyan-600">Objecte</h2>
+
+                    <p class="text-lg text-gray-500 mt-4">JRiba SL(Des d’ara també el prestador) com
+                        responsable del lloc web, JRIBA.ME posa a disposició dels usuaris el present
+                        document, que regula l’ús del lloc web amb el qual pretenem donar compliment a les
+                        obligacions que disposa la Llei 34/2002, de Serveis de la Societat de la Informació i
+                        del Comerç Electrònic (LSSICE o LSSI), així com informar a tots els usuaris del lloc web
+                        sobre quines són les condicions d’ús del lloc web.</p>
+
+                    <p class="text-lg text-gray-500 mt-4">A través del web, JRiba SL facilita als
+                        usuaris
+                        l’accés i utilització de diferents serveis i continguts posats a disposició a través del
+                        web.</p>
+
+                    <p class="text-lg text-gray-500 mt-4">Tota persona que accedeixi a aquest web assumeix el
+                        paper
+                        d’usuari (en endavant l’usuari), i implica l’acceptació total I sense reserves de totes
+                        I cadascuna de les disposicions incloses en aquest avís legal, així com a qualssevol
+                        altres disposicions legals que fossin d’aplicació.</p>
+
+                    <p class="text-lg text-gray-500 mt-4">Com a usuaris, han de llegir atentament aquest Avís
+                        Legal
+                        en qualsevol de les ocasions que entrin al web, ja que aquesta pot patir modificacions
+                        ja que el prestador es reserva el dret a modificar qualsevol tipus d’informació que
+                        pogués aparèixer a la web, sense que hi hagi l’obligació de pre-avisar o posar en
+                        coneixement dels usuaris aquestes obligacions, sent suficients la publicació al lloc web
+                        del prestador.</p>
+
+
+                    <h2 class="text-xl font-semibold mt-5 text-cyan-600">Exclusió de garanties i
+                        responsabilitat</h2>
+
+                    <p class="text-lg text-gray-500 mt-4">El prestador s’eximeix de qualsevol tipus de
+                        responsabilitat derivada de la informació publicada al nostre lloc web, sempre que
+                        aquesta informació hagi estat manipulada o introduïda per un tercer aliè.</p>
+
+                    <p class="text-lg text-gray-500 mt-4">JRiba SL no atorga cap garantia ni es fa
+                        responsable, en cap cas, dels danys i perjudicis de qualsevol naturalesa que puguin
+                        sortir a col·lació de la manca de disponibilitat, manteniment i efectiu funcionament del
+                        web o dels seus serveis i continguts; de l’existència de virus, programes maliciosos o
+                        lesius en els continguts; de l’ús il·lícit, negligent, fraudulent o contrari a aquest
+                        Avís Legal I condicions d’ús; o de la falta de licitud, qualitat, fiabilitat, utilitat i
+                        disponibilitat dels serveis prestats per tercers i posats a disposició dels usuaris al
+                        lloc web.</p>
+
+
+                    <h2 class="text-xl font-semibold mt-5 text-cyan-600">Cookies</h2>
+
+                    <p class="text-lg text-gray-500">El lloc web del prestador pot utilitzar cookies (petits
+                        arxius d’informació que el servidor envia a l’ordinador de qui accedeix a la pàgina) per
+                        dur a terme determinades funcions que són considerades imprescindibles per al correcte
+                        funcionament i visualització del lloc. Les cookies utilitzades tenen, en tot cas,
+                        caràcter temporal, amb l’única finalitat de fer més eficaç la navegació, i desapareixen
+                        en acabar la sessió de l’usuari.
+                    </p>
+                    <p class="text-lg text-gray-500 mt-4">
+                        En cap cas s’utilitzaran les cookies per recollir informació de caràcter personal.
+                    </p>
+
+                    <h2 class="text-xl font-semibold mt-5 text-cyan-600">Protecció de dades personals</h2>
+
+                    <p class="text-lg text-gray-500">El prestador està profundament compromès amb el compliment
+                        de la normativa de protecció de dades personals i garanteix el compliment íntegre de les
+                        obligacions disposades, així com la implementació de les mesures de seguretat disposades
+                        al Reglament Europeu de Protecció de dades i la normativa espanyola de protecció de
+                        dades. Per a més informació, veure la nostra Política de Privacitat.
+                    </p>
+
+                    <h2 class="text-xl font-semibold mt-5 text-cyan-600">Propietat intel·lectual I
+                        industrial</h2>
+
+                    <p class="text-lg text-gray-500 mt-4">El lloc web, incloent a títol enunciatiu però no
+                        limitatiu la programació, edició, compilació i altres elements necessaris per al seu
+                        funcionament, els dissenys, logotips, textos, vídeos, fotografies i / o gràfics són
+                        propietat del prestador o, en cas que fos necessari, disposa de la llicència o
+                        autorització expressa per part dels autors. Tots els continguts del lloc web es troben
+                        degudament protegits per la normativa de propietat intel·lectual i industrial.</p>
+
+                    <p class="text-lg text-gray-500 mt-4">Independentment de la finalitat per la qual fossin
+                        destinats, la reproducció total o parcial, ús, distribució i comunicació pública
+                        requereixen de l’autorització escrita prèvia per part del prestador. Qualsevol ús no
+                        autoritzat prèviament per part del prestador serà considerat un incompliment greu dels
+                        drets de propietat intel·lectual o industrial de l’autor.</p>
+
+                    <h2 class="text-xl font-semibold mt-5 text-cyan-600">Llei aplicable i jurisdicció</h2>
+
+                    <p class="text-lg text-gray-500 mt-4">Per a la resolució de les controvèrsies o qüestions
+                        relacionades amb la present pàgina web o de les activitats en aquesta desenvolupades,
+                        serà d’aplicació la legislació espanyola, a la qual se sotmeten expressament les parts,
+                        sent competents per a la resolució de tots els conflictes derivats o relacionats amb el
+                        seu ús els Jutjats i Tribunals.</p>
+
+                    <h2 class="text-xl font-semibold mt-5 text-cyan-600">Veracitat de la informació</h2>
+
+                    <p class="text-lg text-gray-500 mt-4">Tota la informació que facilita l’Usuari ha de ser
+                        veraç. A aquests efectes, l’Usuari garanteix l’autenticitat de les dades comunicades a
+                        través dels formularis de contacte.
+                    </p>
+
+                    <p class="text-lg text-gray-500 mt-4">Serà responsabilitat de l’Usuari mantenir tota la
+                        informació facilitada a ------- SL permanentment actualitzada de manera que
+                        respongui, en cada moment, a la seva situació real. En tot cas, l’Usuari serà l’únic
+                        responsable de les manifestacions falses o inexactes que realitzi i dels perjudicis que
+                        causi al prestador o a tercers.</p>
+
+                </div>
+            </div>
+        </div>
+    </main>
+    </div>
+    </div>
+
+</x-guest-layout>
